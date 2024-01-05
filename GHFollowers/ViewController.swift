28509bdcbce96b23848d06a421d53d0580b389eb
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2024-01-06 01:48:18 +0530

    Project Added

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Pulkit Dhirana on 06/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
